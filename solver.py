7e08af357ded94018e065032aed780cceb3aa278
Nilesh Kumar <nileshkumar98@outlook.com>
2020-09-21 19:01:20 +0530

    Integrated gui with backtracking function

--- solver.py
+++ solver.py
@@ -3,7 +3,7 @@
     function empty_position takes the board(sudoku matrix) as empty and returns the first empty position.
     when there is no empty position, it returns None.
 '''
-
+from functions import *
 def empty_position(board):
     for i in  range(len(board)):
         for j in range(len(board[0])):
@@ -16,7 +16,8 @@ Function valid_number takes board, number and position(row,column) in the sudoku
 it return True if the value is not present alredy in row or colum or 3*3 box
 else it return False
 """
-def valid_number(board,num,position):
+
+def valid(board,num,position):
     row,col = position
 
     for i in range(len(board[0])):
@@ -48,25 +49,22 @@ if sudoku can't be solved it resets the position and backtracks.
 """
 def solve(board):
     pos = empty_position(board) #check for the empty position
-
     # if there is no empty position return true i.e board is solved
     if pos == None:
         return True
-    
     # iterate for values ranging in 1 to 9
     for value in range(1,10):
-        if valid_number(board,value,pos): #if a value is valid update the position
+        if valid(board,value,pos): #if a value is valid update the position
             row,col = pos
             board[row][col] = value
-
             if solve(board): #recurse
                 return True
             else:
                 board[row][col] = 0 #reset the position
-
+        draw_grid(board,screen)
     return False
 
-board = [[5,3,0,0,7,0,0,0,0],
+'''board = [[5,3,0,0,7,0,0,0,0],
         [6,0,0,1,9,5,0,0,0],
         [0,9,8,0,0,0,0,6,0],
         [8,0,0,0,6,0,0,0,3],
@@ -74,6 +72,31 @@ board = [[5,3,0,0,7,0,0,0,0],
         [7,0,0,0,2,0,0,0,6],
         [0,6,0,0,0,0,2,8,0],
         [0,0,0,4,1,9,0,0,5],
-        [0,0,0,0,8,0,0,7,9]]
+        [0,0,0,0,8,0,0,7,9]]'''
 
-solve(board)
+board = [ 
+		[7, 8, 0, 4, 0, 0, 1, 2, 0], 
+		[6, 0, 0, 0, 7, 5, 0, 0, 9], 
+		[0, 0, 0, 6, 0, 1, 0, 7, 8], 
+		[0, 0, 7, 0, 4, 0, 2, 6, 0], 
+		[0, 0, 1, 0, 5, 0, 9, 3, 0], 
+		[9, 0, 4, 0, 6, 0, 0, 0, 5], 
+		[0, 7, 0, 3, 0, 0, 0, 1, 2], 
+		[1, 2, 0, 0, 0, 7, 4, 0, 0], 
+		[0, 4, 9, 2, 0, 6, 0, 0, 7] 
+	]
+
+#solve(board)
+
+
+def print_sudoku(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if (j+1)%3 == 0 and j != 0:
+                print(" " + str(board[i][j]) + "| ", end = "")
+            else:
+                print(" " + str(board[i][j]) + " ", end = "")
+        print("\n")
+        if (i+1)%3 == 0 and i != 0:
+            print("-----------------------------")
+print_sudoku(board)
