f72181885908c9e0ca02063f19035d9db11bc634
Nilesh Kumar <nileshkumar98@outlook.com>
2020-09-21 18:55:37 +0530

    Removed redundant code

--- gui.py
+++ gui.py
@@ -2,36 +2,27 @@ import pygame
 from solver import solve, valid
 import time
 pygame.font.init()
+from functions import *
+#default sudoku grid
+grid = [ 
+		[7, 8, 0, 4, 0, 0, 1, 2, 0], 
+		[6, 0, 0, 0, 7, 5, 0, 0, 9], 
+		[0, 0, 0, 6, 0, 1, 0, 7, 8], 
+		[0, 0, 7, 0, 4, 0, 2, 6, 0], 
+		[0, 0, 1, 0, 5, 0, 9, 3, 0], 
+		[9, 0, 4, 0, 6, 0, 0, 0, 5], 
+		[0, 7, 0, 3, 0, 0, 0, 1, 2], 
+		[1, 2, 0, 0, 0, 7, 4, 0, 0], 
+		[0, 4, 9, 2, 0, 6, 0, 0, 7] 
+	]
 
 
-grid = [[5,3,0,0,7,0,0,0,0],
-        [6,0,0,1,9,5,0,0,0],
-        [0,9,8,0,0,0,0,6,0],
-        [8,0,0,0,6,0,0,0,3],
-        [4,0,0,8,0,3,0,0,1],
-        [7,0,0,0,2,0,0,0,6],
-        [0,6,0,0,0,0,2,8,0],
-        [0,0,0,4,1,9,0,0,5],
-        [0,0,0,0,8,0,0,7,9]]
-screen = pygame.display.set_mode((500,600))
-pygame.display.set_caption("Sudoku Solver")
-
-#Draw the Sudoku Grids
-dif = 500/9
-font1 = pygame.font.SysFont("comicsans", 40) 
-font2 = pygame.font.SysFont("comicsans", 20)
-color = (255,255,255)
-for i in range (9): 
-	for j in range (9): 
-		if grid[i][j]!= 0: 
-			pygame.draw.rect(screen, (0, 153, 153), (i * dif, j * dif, dif + 1, dif + 1)) 
-			text1 = font1.render(str(grid[i][j]), 1, (0, 0, 0)) 
-			screen.blit(text1, (i * dif + 15, j * dif + 15))
-for i in range(10): 
-	if i % 3 == 0 : 
-		thick = 7
-	else: 
-		thick = 1
-	pygame.draw.line(screen, color, (0, i * dif), (500, i * dif), thick) 
-	pygame.draw.line(screen, color, (i * dif, 0), (i * dif, 500), thick)
-pygame.display.update()
+run = True
+while run:
+    draw_grid(grid,screen)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run =  False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            solve(grid)
